346b25601b2e49bde73110ab95bcadaefd5bdd59
zzb <bymiracles@163.com>
2023-12-21 10:10:53 +0800

    图片更新

--- market/market/apps/meiduo_admin/serialziers/ImageSerializer.py
+++ market/market/apps/meiduo_admin/serialziers/ImageSerializer.py
@@ -28,3 +28,21 @@ class ImageSerializer(serializers.ModelSerializer):
         # 保存图片
         img = SKUImage.objects.create(sku_id=sku_id, image=image_url)
         return img
+
+    def update(self, instance: SKUImage, validated_data):
+        # 创建FastDFS连接对象
+        client = Fdfs_client(settings.FASTDFS_PATH)
+        # 获取前端传递的image文件
+        data = self.context['request'].FILES.get('image')
+        # 上传图片到fastDFS
+        res = client.upload_by_buffer(data.read())
+        # 判断是否上传成功
+        if res['Status'] != 'Upload successed.':
+            return Response(status=403)
+        # 获取上传后的路径
+        image_url = res['Remote file_id']
+        # 保存图片
+        instance.image = image_url
+        # 数据库保存
+        instance.save()
+        return instance
